28eaae796b0ff02d5dc7f5ca27c42bde4aaabdaa
Vivek Sah <vivek.sah32+github@gmail.com>
2021-04-25 15:20:05 -0700

    refactor to use model in view function

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -2,21 +2,24 @@ module PhotoGroove exposing (main)
 
 import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
+import List
 
+urlPrefix = "http://elm-in-action.com/"
 
 view model =
      div [ class "content"]
          [ h1 [ ] [text "Photo Groove"]
-         , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg"] []
-            , img [ src "http://elm-in-action.com/2.jpeg"] []
-            , img [ src "http://elm-in-action.com/3.jpeg"] []
-            ]
+         , div [ id "thumbnails" ] (List.map showThumbnails model)
          ]
 
 
+showThumbnails imgRecord = img [src (urlPrefix ++ imgRecord.url)] []
 
-
+initialModel = [
+            {url = "1.jpeg"}
+            , {url = "2.jpeg"}
+            , {url = "3.jpeg"}
+            ]
 
 main =
-    view "no model yet"
+    view initialModel
