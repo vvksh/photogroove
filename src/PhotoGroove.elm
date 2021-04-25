52231a6a0406b078a6b0cad1bd85d227dce2e5fb
Vivek Sah <vivek.sah32+github@gmail.com>
2021-04-25 15:41:52 -0700

    Add select and preview

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -3,23 +3,53 @@ module PhotoGroove exposing (main)
 import Html exposing (div, h1, img, text)
 import Html.Attributes exposing (..)
 import List
+import Html.Events exposing (onClick)
+import Browser
 
 urlPrefix = "http://elm-in-action.com/"
 
 view model =
      div [ class "content"]
          [ h1 [ ] [text "Photo Groove"]
-         , div [ id "thumbnails" ] (List.map showThumbnails model)
+         , div [ id "thumbnails" ]
+                (List.map (showThumbnails model.selectedUrl) model.photos)
+         , img
+            [
+            class "large"
+            , src (urlPrefix ++ model.selectedUrl)
+            ]
+            []
          ]
 
+update message model =
+    if message.description == "ClickedPhoto" then
+        {model | selectedUrl = message.data }
+    else
+        model
 
-showThumbnails imgRecord = img [src (urlPrefix ++ imgRecord.url)] []
 
-initialModel = [
+showThumbnails selectedUrl imgRecord =
+    img
+        [
+        classList [("selected", selectedUrl == imgRecord.url)]
+        , src (urlPrefix ++ imgRecord.url)
+        , onClick {description = "ClickedPhoto", data = imgRecord.url}
+        ]
+        []
+
+initialModel =
+    {
+        photos = [
             {url = "1.jpeg"}
             , {url = "2.jpeg"}
             , {url = "3.jpeg"}
-            ]
+        ]
+        , selectedUrl = "1.jpeg"
+    }
 
 main =
-    view initialModel
+    Browser.sandbox
+    {init = initialModel
+    , view = view
+    , update = update
+    }
